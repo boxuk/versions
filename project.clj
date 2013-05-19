82254ea24c125e56ed4da8e6e2c5f590aff9fdf2
rod <rod@pu-gh.com>
2013-05-19 22:59:55 +0100

    bumped clojure version to 1.5.1

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 
 (defproject boxuk.versions "0.1.0"
   :description "Tool for comparing version numbers"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]])
 
